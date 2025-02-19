fd27e0c1c88f0d04be38aa594a7690cf8b2b7d32
Enrick Santos <enricksantos03@gmail.com>
2025-02-19 15:25:09 -0300

    chore: adicionado isLoading nos produtos

--- public/app.js
+++ public/app.js
@@ -1,9 +1,20 @@
 // URL base do seu backend
 const BASE_URL = 'http://localhost:3000/api/products';
 
-function renderProducts(products) {
+function renderProducts(products, isLoading = false) {
   const productsList = document.getElementById('productsList');
   productsList.innerHTML = '';
+
+  if (isLoading) {
+    Array.from({ length: 20 }, () => {
+      const productCard = document.createElement('div');
+      productCard.className =
+        'border border-gray-200 rounded-lg h-[314px] p-4 rounded shadow transition bg-gray-200';
+
+      productsList.appendChild(productCard);
+    });
+    return;
+  }
 
   if (products.length === 0) {
     productsList.innerHTML = `<p class="col-span-full">Nenhum produto encontrado.</p>`;
@@ -13,11 +24,16 @@ function renderProducts(products) {
   products.forEach((product) => {
     const productCard = document.createElement('div');
     productCard.className =
-      'bg-white border border-gray-100 rounded-lg p-4 rounded shadow hover:shadow-lg transition';
+      'bg-white border border-gray-100 rounded-lg p-4 rounded shadow transition';
 
     productCard.innerHTML = `
-      <h2 class="text-xl font-semibold mb-2">${product.name}</h2>
-      <p class="text-gray-600">${product.description || ''}</p>
+      <div class="w-full h-40 bg-gray-200 rounded-lg"></div>
+      <h2 class="text-xl font-semibold mt-2">${product.name}</h2>
+      <p class="text-gray-600">${product.desc || ''}</p>
+      <div class="flex justify-between gap-2 items-center mt-3">
+        <p class="font-medium">${product.pricing.price_usd}</p>
+        <p class="text-xs text-gray-500" >${product.category}</p>
+      </div>
     `;
 
     productsList.appendChild(productCard);
@@ -32,6 +48,7 @@ async function fetchProducts(keyword = '', cep = '') {
     if (cep) params.append('cep', cep);
     if (params.toString()) url += `?${params.toString()}`;
 
+    renderProducts([], true);
     const response = await fetch(url);
     if (!response.ok) throw new Error('Erro ao buscar produtos');
     const data = await response.json();
