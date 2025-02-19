866a25dccbf829edaf6b65421e223b1a40bb4f3b
Enrick Santos <enricksantos03@gmail.com>
2025-02-19 10:43:54 -0300

    feat: servir aquivivos estaticos do diretorio 'public'

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
