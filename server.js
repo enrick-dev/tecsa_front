ace6b198563191f2112008c57195c9500d50af63
Enrick Santos <enricksantos03@gmail.com>
2025-02-19 12:48:12 -0300

    feat: adicionado requisição findProducts que retorna o produto com seu estado

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
-const express = require('express');
-const path = require('path');
+import 'dotenv/config';
+import axios from 'axios';
+import express from 'express';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express();
+export const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default class Service {
+  async findProducts(req, res) {
+    const productsResponse = await axios.get(process.env.PRODUCTS_API_URL, {
+      headers: {
+        Authorization: process.env.PRODUCTS_API_KEY,
+      },
+    });
+
+    const products = await Promise.all(
+      productsResponse.data.items.map(async (product) => {
+        if (product.cep) {
+          const cep = product.cep.replace(/\D/g, '');
+          try {
+            const cepResponse = await axios.get(
+              `${process.env.CEP_API_URL}/${cep}/json/`,
+            );
+            product.state = cepResponse.data.uf;
+          } catch (error) {
+            product.state = null;
+          }
+        }
+        return product;
+      }),
+    );
+
+    return res.json(products);
+  }
+}
+
+const service = new Service();
+
+app.get('/api/products', async (req, res) => {
+  return service.findProducts(req, res);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
